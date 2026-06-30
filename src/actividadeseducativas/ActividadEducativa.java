45fef51c7f86ba3aceab04d15ecd49f76dc6169d
Dylan <dylanpabri@gmail.com>
2026-06-30 15:28:05 -0600

    Se crea la clase /ActividadEducativa y el ENUM /TipoActividades
    Se crea la clase con sus atributos, sus metodos get, metodos set, constructor, toString, falta agregar metodods funcionales

---
+++ src/actividadeseducativas/ActividadEducativa.java
@@ -0,0 +1,76 @@
+
+package actividadeseducativas;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Dylan
+ */
+public class ActividadEducativa {
+    
+TipoActividades nombreActividad;   
+private LocalDate fecha;        
+private LocalDate hora;        
+private int[] cupoMax;
+Usuario usuarioInscrito;
+
+    public TipoActividades getNombreActividad() {
+        return nombreActividad;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public LocalDate getHora() {
+        return hora;
+    }
+
+    public int[] getCupoMax() {
+        return cupoMax;
+    }
+
+    public Usuario getUsuarioInscrito() {
+        return usuarioInscrito;
+    }
+
+    public void setNombreActividad(TipoActividades nombreActividad) {
+        this.nombreActividad = nombreActividad;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setHora(LocalDate hora) {
+        this.hora = hora;
+    }
+
+    public void setCupoMax(int[] cupoMax) {
+        this.cupoMax = cupoMax;
+    }
+
+    public void setUsuarioInscrito(Usuario usuarioInscrito) {
+        this.usuarioInscrito = usuarioInscrito;
+    }
+
+   
+    
+    
+    public ActividadEducativa(TipoActividades nombreActividad, LocalDate fecha, LocalDate hora, int[] cupoMax, Usuario usuarioInscrito) {
+        this.nombreActividad = nombreActividad;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.cupoMax = cupoMax;
+        this.usuarioInscrito = usuarioInscrito;
+    }
+
+    @Override
+    public String toString() {
+        return "ActividadEducativa: "  + nombreActividad + " Fecha: " + fecha + " Hora: " + hora + " CupoMax: " + cupoMax + "Usuario Inscrito: " + usuarioInscrito;
+    }
+
+
+
+}
---
+++ src/actividadeseducativas/TipoActividades.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package actividadeseducativas;
+
+/**
+ *
+ * @author Student
+ */
+public enum TipoActividades {
+    TALLER_LECTURA,
+    CLUB_LIBRO,
+    CHARLAS;
+}
